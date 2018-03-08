d59cf0433d1f2c06ac12d160876a45431e8c2c98
gabor930 <kissgabor930@gmail.com>
2018-03-08 18:32:08 +0100

    Book setters & getters

--- core/src/main/java/org/web/core/Book.java
+++ core/src/main/java/org/web/core/Book.java
@@ -10,4 +10,73 @@ public class Book {
     private Publisher publisher;
     private Date publicationDate;
     private Nationality language;
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public void setLanguage(Nationality language) {
+        this.language = language;
+    }
+
+    public int getISBN(){
+        return ISBN;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public Nationality getLanguage() {
+        return language;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "ISBN=" + ISBN +
+                ", name='" + name + '\'' +
+                ", subject=" + subject +
+                ", author=" + author +
+                ", publisher=" + publisher +
+                ", publicationDate=" + publicationDate +
+                ", language=" + language +
+                '}';
+    }
 }
