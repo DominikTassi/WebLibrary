b79ea13049c5e2ad3aa414af0304edec4930cd63
gabber <kissgabor930@gmail.com>
2018-04-04 12:11:46 +0200

    test class

---
+++ dao/src/main/java/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        DataBaseInit dataBaseInit = new DataBaseInit();
+        System.out.println("test");
+        dataBaseInit.init();
+    }
+}
