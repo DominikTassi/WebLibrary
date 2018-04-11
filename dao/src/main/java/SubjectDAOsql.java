045b6835896939b091ac05e5a93e7c1b103290d9
Tassi Dominik <doma_tassi@hotmail.com>
2018-04-11 11:48:03 +0200

    Started SubjectDAO
    hope it works

---
+++ dao/src/main/java/SubjectDAOsql.java
@@ -0,0 +1,91 @@
+import org.web.core.exceptions.NoNameException;
+import org.web.core.exceptions.NoSubjectException;
+import org.web.core.model.Subject;
+import org.web.service.dao.SubjectDAO;
+
+import java.sql.*;
+import java.util.Collection;
+
+public class SubjectDAOsql extends DataBaseInit implements SubjectDAO {
+
+    private Connection connection = openConnection();
+    private String url = DataBaseInit.url;
+
+    public SubjectDAOsql() throws SQLException, ClassNotFoundException {
+        DataBaseInit.getInstance();
+    }
+
+    @Override
+    public Subject getSubject(int id) throws NoSubjectException {
+        Subject subject = null;
+        try{
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection(url);
+            connection.setAutoCommit(false);
+
+            int subjectId = 0;
+            String name = null;
+
+
+            String sql = "SELECT * FROM User WHERE SubjectId = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1,id);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                subjectId = rs.getInt("SubjectId");
+                name = rs.getString("Name");
+            }
+            subject = new Subject(subjectId, name);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }catch (NoNameException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return subject;
+    }
+
+    @Override
+    public Subject getSubject(Subject subject) throws NoSubjectException {
+        return null;
+    }
+
+    @Override
+    public Collection<Subject> getAllSubject() {
+        return null;
+    }
+
+    @Override
+    public void addSubject(Subject subject) {
+        try{
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection(url);
+            connection.setAutoCommit(false);
+            String sql = "INSERT INTO User (SubjectId, Name) VALUES(?, ?)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, subject.getId());
+            ps.setString(2, subject.getName());
+            ps.executeUpdate();
+            connection.commit();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            connection = null;
+        }
+
+    }
+}
