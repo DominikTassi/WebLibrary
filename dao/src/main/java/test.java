f5b4d4763261c83bc3dfca265d39c0606883816c
gabor930 <kissgabor930@gmail.com>
2018-04-12 14:08:45 +0200

    test file

--- dao/src/main/java/test.java
+++ dao/src/main/java/test.java
@@ -1,18 +1,23 @@
 import org.web.core.exceptions.NoNameException;
 import org.web.core.exceptions.NoPasswordException;
 import org.web.core.exceptions.NoUserException;
+import org.web.core.exceptions.WrongISBNException;
+import org.web.core.model.Book;
 import org.web.core.model.User;
 
 import java.sql.SQLException;
 
 public class test {
-    public static void main(String[] args) throws NoPasswordException, NoNameException, SQLException, ClassNotFoundException, NoUserException {
+    public static void main(String[] args) throws NoPasswordException, NoNameException, SQLException, ClassNotFoundException, NoUserException, WrongISBNException {
         DataBaseInit dataBaseInit = new DataBaseInit();
-        UserDAOSql userDAOSql = new UserDAOSql();
+        //UserDAOSql userDAOSql = new UserDAOSql();
+        BookDAOsql bookDAOsql = new BookDAOsql();
 
         System.out.println("test");
-        User user = new User(1, "name1", "password");
-        User user2 = new User(2, "name2", "password3");
-        System.out.println(userDAOSql.getUser(1).toString());
+
+
+        Book book = bookDAOsql.getBookByISBN(1);
+        System.out.println(book.toString());
+        System.out.println(book.getPublisher().getName());
     }
 }
